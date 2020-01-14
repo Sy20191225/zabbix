19126cb2863188c3518f68189c16824a6adcbda8
yottachain <yushuai@yottachain.io>
2020-01-14 19:31:55 +0800

    up node

--- script/node_votes.sh
+++ script/node_votes.sh
@@ -2,7 +2,8 @@
 ### 监控主备超级节点票数
 ### sh node_votes.sh m|b
 
-node_votes=( $(/app/bp/nodeos/cleos -u http://47.93.13.197:8888/ get table eosio eosio prodslevel |grep total_votes |awk -F'[".]' '{print $4}') )
+#node_votes=( $(/app/bp/nodeos/cleos -u http://47.93.13.197:8888/ get table eosio eosio prodslevel |grep total_votes |awk -F'[".]' '{print $4}') )
+node_votes=( $(/app/bp/nodeos/cleos -u http://127.0.0.1:7000/ get table eosio eosio prodslevel |grep total_votes |awk -F'[".]' '{print $4}') )
 
 m_node() {
     for (( i=0;i<21;i++))
